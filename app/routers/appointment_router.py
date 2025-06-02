3cbf34599f8c0291f4f19067b3942a71caa0ba29
Kunal7069 <jaink7069@gmail.com>
2025-06-02 18:44:09 +0530

    appointments by user name

--- app/routers/appointment_router.py
+++ app/routers/appointment_router.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-
+from pydantic import BaseModel
 from app.database.config import SessionLocal
 from app.schemas.appointment_schema import AppointmentCreate, AppointmentResponse
-from app.services.appointment_service import create_appointment
+from app.services.appointment_service import create_appointment,get_appointments_by_patient_name
 
 router = APIRouter(prefix="/appointment", tags=["Appointments"])
 
@@ -17,3 +17,13 @@ def get_db():
 @router.post("/", response_model=AppointmentResponse)
 def book_appointment(appointment: AppointmentCreate, db: Session = Depends(get_db)):
     return create_appointment(db, appointment.dict())
+
+class PatientNameRequest(BaseModel):
+    patient_name: str
+
+@router.post("/by-patient")
+def get_appointments_for_patient(data: PatientNameRequest, db: Session = Depends(get_db)):
+    appointments = get_appointments_by_patient_name(db, data.patient_name)
+    if not appointments:
+        raise HTTPException(status_code=404, detail="No appointments found for this patient.")
+    return appointments
--- app/services/appointment_service.py
+++ app/services/appointment_service.py
@@ -1,10 +1,21 @@
 
 from sqlalchemy.orm import Session
 from app.models.appointment import DoctorAppointment
-
+from sqlalchemy.orm import joinedload
 def create_appointment(db: Session, appointment_data: dict):
     appointment = DoctorAppointment(**appointment_data)
     db.add(appointment)
     db.commit()
     db.refresh(appointment)
     return appointment
+
+# def get_appointments_by_patient_name(db: Session, patient_name: str):
+#     return db.query(DoctorAppointment).filter(DoctorAppointment.patient_name == patient_name).all()
+
+def get_appointments_by_patient_name(db: Session, patient_name: str):
+    return (
+        db.query(DoctorAppointment)
+        .options(joinedload(DoctorAppointment.doctor), joinedload(DoctorAppointment.slot))
+        .filter(DoctorAppointment.patient_name == patient_name)
+        .all()
+    )
