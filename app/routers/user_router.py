be701ffc26bbf1afff229d0d6d514e32edb3eee7
Kunal7069 <jaink7069@gmail.com>
2025-06-02 17:31:53 +0530

    login done

--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from app.database.config import SessionLocal
-from app.schemas.user_schema import UserCreate, UserResponse
-from app.services.user_service import create_user
+from app.schemas.user_schema import UserCreate, UserResponse,UserLogin
+from app.services.user_service import create_user,login_user
 
 router = APIRouter(prefix="/user", tags=["User"])
 
@@ -18,3 +18,7 @@ def get_db():
 @router.post("/", response_model=UserResponse)
 def register_user(user: UserCreate, db: Session = Depends(get_db)):
     return create_user(db, user.dict())
+
+@router.post("/login", response_model=UserResponse)
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    return login_user(db, user.email, user.password)
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,9 +1,20 @@
 from sqlalchemy.orm import Session
 from app.models.user import User
+from fastapi import HTTPException, status
+
 
 def create_user(db: Session, user_data: dict):
     user = User(**user_data)
     db.add(user)
     db.commit()
     db.refresh(user)
+    return user
+
+def login_user(db: Session, email: str, password: str):
+    user = db.query(User).filter(User.email == email).first()
+    if not user or user.password != password:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid email or password"
+        )
     return user
