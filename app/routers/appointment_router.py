0a9b971238a0f7beda37490a0fe7515c6d81b1d0
Kunal7069 <jaink7069@gmail.com>
2025-06-02 21:28:36 +0530

    bug fixed

--- app/routers/appointment_router.py
+++ app/routers/appointment_router.py
@@ -3,8 +3,9 @@ from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from app.database.config import SessionLocal
 from app.schemas.appointment_schema import AppointmentCreate, AppointmentResponse
+from app.models.doctor import Doctor
 from app.services.appointment_service import create_appointment,get_appointments_by_patient_name,get_appointments_by_doctor_id
-
+from app.services.doctor_service import get_all_doctors_with_slots
 router = APIRouter(prefix="/appointment", tags=["Appointments"])
 
 def get_db():
@@ -29,11 +30,13 @@ def get_appointments_for_patient(data: PatientNameRequest, db: Session = Depends
     return appointments
 
 class DoctorIdRequest(BaseModel):
-    doctor_id: int
+    doctor_name: str
 
 @router.post("/by-doctor")
 def get_appointments_for_doctor(data: DoctorIdRequest, db: Session = Depends(get_db)):
-    appointments = get_appointments_by_doctor_id(db, data.doctor_id)
+    doctor = db.query(Doctor).filter(Doctor.doctor_name == data.doctor_name).first()
+
+    appointments = get_appointments_by_doctor_id(db, doctor.id)
     if not appointments:
         raise HTTPException(status_code=404, detail="No appointments found for this doctor.")
     return appointments
